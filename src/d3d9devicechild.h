138a1f6ee2791a15255662da5e201a2997aaaeaf
Isja <165357593+isja13@users.noreply.github.com>
2026-02-23 20:53:12 -0600

    Delete src directory

--- src/d3d9devicechild.h
+++
@@ -1,9 +0,0 @@
-#ifndef D3D9DEVICECHILD_H
-#define D3D9DEVICECHILD_H
-
-#include <d3d9.h>
-#include "macros.h"
-
-
-
-#endif // D3D9DEVICECHILD_H
--- src/slang_d3d9_preset_load.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "d3d9video.h"
-
-namespace ZeroMod {
-	bool slang_d3d9_load_preset_parse_only(d3d9_video_struct* d3d9);
-}
-
